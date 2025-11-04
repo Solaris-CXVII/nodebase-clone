7a1e74cc58acfb48b5577889d3c5cd1bca3cde4f
Solaris-CXVII <aldencua2403@gmail.com>
2025-11-05 07:54:06 +0800

    Install prisma for database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
-  return <div className="text-red-500 font-bold">Hello World</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button variant={"ghost"}>Button</Button>
+    </div>
+  );
 };
 
 export default Page;
